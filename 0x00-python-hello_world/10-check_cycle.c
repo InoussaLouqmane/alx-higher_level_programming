7dd62818c89f2572977dd59ab772b930f45daa64
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-03-11 22:51:10 +0100

    Code optimization

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -16,15 +16,12 @@ int check_cycle(listint_t *list)
 	listint_t *tortoise = list;
 	listint_t *fox = list;
 
-	if (list != NULL)
+	while (tortoise != NULL && fox != NULL && fox->next != NULL)
 	{
-		while ((tortoise->next) != NULL && (fox->next)->next != NULL)
-		{
-			tortoise = tortoise->next;
-			fox = (fox->next)->next;
-			if (fox == tortoise)
-				return (1);
-		}
-	}
+		tortoise = tortoise->next;
+		fox = (fox->next)->next;
+		if (fox == tortoise)
+			return (1);
+	}	
 	return (0);
 }
