483633b4a22fb753479b63926d6a52324e89d86e
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-03-13 06:18:59 +0100

    Prints combinaison

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range (0, 10):
+        if j > i:
+            print(f"{i}{j}", end="")
+            if i != 8:
+                print("", end=", ")
+print("")
+
