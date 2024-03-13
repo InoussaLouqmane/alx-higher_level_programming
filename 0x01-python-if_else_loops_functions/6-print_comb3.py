d204fd91196a552b72c04d39a6897817dac0dc4b
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-03-13 11:41:42 +0100

    Corrections on several files

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 9):
     for j in range (0, 10):
         if j > i:
-            print(f"{i}{j}", end="")
+            print("{}{}".format(i, j), end="")
             if i != 8:
                 print("", end=", ")
 print("")
