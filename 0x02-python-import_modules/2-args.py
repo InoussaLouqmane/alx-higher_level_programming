b94a4d65c24a5d755031057448dc04f958650a30
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-03-14 10:45:38 +0100

    A dynamic scriipt

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    ac = len(argv)
+    if ac == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(ac - 1))
+        for i in range(1, ac):
+            print("{}: {}".format(i, argv[i]))
+
