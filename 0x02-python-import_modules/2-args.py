ce325749a567ce6c50553136ce722fc971f8bdbe
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-03-14 17:15:23 +0100

    Output correction for file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,11 @@ if __name__ == "__main__":
     ac = len(argv)
     if ac == 1:
         print("0 arguments.")
+    elif ac == 2:
+        print("1 argument:")
     else:
         print("{} arguments:".format(ac - 1))
+    if ac >= 2:
         for i in range(1, ac):
             print("{}: {}".format(i, argv[i]))
 
