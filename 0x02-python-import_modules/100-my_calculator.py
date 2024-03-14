aef34620bfbf222ec38a5c4bacf48a8363fcb915
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-03-14 18:00:01 +0100

    parsing code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
-    argv = sys.argv
+from calculator_1 import add, sub, mul, div
+
+def calcul(argv):
     ac = len(argv)
     a = 0
     b = 0
-
     if ac != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     a = int(argv[1])
     b = int(argv[3])
     operator = argv[2]
-
     if operator not in ['+','-','*','/']:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
     if operator == '+':
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == '-':
@@ -27,3 +22,8 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
     else:
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+
+
+if __name__ == "__main__":
+    import sys
+    calcul(sys.argv)
