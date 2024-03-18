47083d658b24bc23cda875c575cd2f37fa0df0f9
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-03-18 18:41:46 +0100

    fixing loops

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        print("{:d} {:d} {:d}".format(i[0], i[1], i[2]), )
+        for j in i:
+            print("{:d}".format(j), end=' ')
+        print("")
